9329af8a0e80238513fbebb9e60a70c8ac79621f
1Consumption <hanseop95@gmail.com>
2020-03-30 18:48:32 +0900

    [Feat] #9 프로젝트 생성

---
+++ iOS/dust/dust/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  dust
+//
+//  Created by 신한섭 on 2020/03/30.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
