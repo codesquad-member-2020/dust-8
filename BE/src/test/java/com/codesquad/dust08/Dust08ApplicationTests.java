5e3aa16041352e14f43fb8211f8e0452897adc18
ragdoll <unknown>
2020-03-30 18:35:09 +0900

    [Chore] 프로젝트 초기화
    Fixes #12

---
+++ BE/src/test/java/com/codesquad/dust08/Dust08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.dust08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dust08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
