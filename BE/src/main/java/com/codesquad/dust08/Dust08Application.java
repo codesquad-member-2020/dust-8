716a1d848359c6fb98f4ddc48461c3d6b268c99c
ragdoll <>
2020-03-30 18:35:09 +0900

    [Chore] 프로젝트 초기화
    Fixes #12

---
+++ BE/src/main/java/com/codesquad/dust08/Dust08Application.java
@@ -0,0 +1,13 @@
+package com.codesquad.dust08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dust08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dust08Application.class, args);
+	}
+
+}
