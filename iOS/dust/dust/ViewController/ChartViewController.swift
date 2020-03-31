4dcd07ec56208e79661d39827e5cdfbadadfcd39
1Consumption <hanseop95@gmail.com>
2020-03-31 22:19:35 +0900

    [Feat] summaryview UI 정보 update
    노티피케이션 받은 모델에서 정보 추출하여 view에 업데이트

--- iOS/dust/dust/ViewController/ChartViewController.swift
+++ iOS/dust/dust/ViewController/ChartViewController.swift
@@ -9,27 +9,33 @@
 import UIKit
 
 class ChartViewController: UIViewController {
-
+    
     @IBOutlet weak var summaryView: SummaryView!
     @IBOutlet weak var chartTableView: UITableView!
+    @IBOutlet weak var emoticonLabel: UILabel!
+    @IBOutlet weak var gradeLabel: UILabel!
+    @IBOutlet weak var numericLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var stationLabel: UILabel!
     
     private var model = [DustInfoModel]()
     private var dataSource = ChartTableViewDatasource()
     private var delegate = ChartTableViewDelegate()
+    private let emoticonUnicode = ["Good" : "\u{1F600}", "Normal" : "\u{1F642}", "Bad" : "\u{1F637}", "Terrible" : "\u{1F631}"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for _ in 0..<24 {
-            model.append(DustInfoModel(numeric: Int.random(in: 0...200)))
+        for index in 0..<24 {
+            model.append(DustInfoModel(numeric: Int.random(in: 0...200), station: "강남구", time: String(index)))
         }
         
-        dataSource.model = model
-        delegate.model = model
-        self.chartTableView.dataSource = dataSource
-        self.chartTableView.delegate = delegate
+        setupDelegate()
+        setupDatasource()
         
-        summaryView.state = model[0].grade
+        self.emoticonLabel.font = UIFont(name: self.timeLabel.font.fontName, size: self.summaryView.frame.height * 0.35)
+        
+        changeSummaryViewUI(model: model[0])
         
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(changeSummaryViewUI(_:)),
@@ -37,9 +43,31 @@ class ChartViewController: UIViewController {
                                                object: nil)
     }
     
+    func setupDelegate() {
+        delegate.model = model
+        self.chartTableView.delegate = delegate
+    }
+    
+    func setupDatasource() {
+        dataSource.model = model
+        self.chartTableView.dataSource = dataSource
+    }
+    
+    func changeSummaryViewUI(model: DustInfoModel) {
+        DispatchQueue.main.async {
+            self.summaryView.state = model.grade
+            self.numericLabel.text = String(model.numeric)
+            self.stationLabel.text = String(model.station)
+            self.timeLabel.text = String(model.time)
+            self.gradeLabel.text = model.grade.rawValue
+            
+            self.emoticonLabel.text = self.emoticonUnicode[model.grade.rawValue]
+        }
+    }
+    
     @objc func changeSummaryViewUI(_ notification: Notification) {
         guard let model = notification.userInfo?["model"] as? DustInfoModel else {return}
-        summaryView.state = model.grade
+        changeSummaryViewUI(model: model)
     }
 }
 
