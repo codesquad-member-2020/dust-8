3c919d74e163c1d2db2234b9ebfe44b1c8e69cda
ragdoll <>
2020-03-31 17:40:28 +0900

    [Fix] Mock API 스켈레톤 코드 수정 Fixed #17 - RequestMapping으로 URL 정리 - RestController로 변경

--- BE/src/main/java/com/codesquad/dust08/controller/ForecaseController.java
+++ BE/src/main/java/com/codesquad/dust08/controller/ForecaseController.java
@@ -1,26 +1,28 @@
 package com.codesquad.dust08.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/forecase")
 public class ForecaseController {
     // 24시간 대기오염 이미지 호출
-    @GetMapping("/forecase/images")
+    @GetMapping("/images")
     public String getOverallImages() {
 
         return "http://www.airkorea.or.kr/file/viewImage/?atch_id=138717";
     }
 
     // 예보 문구 출력 호출
-    @GetMapping("/forecase/informoverall")
+    @GetMapping("/informoverall")
     public String getOverallForecase() {
 
         return "{ \"informOverall\" : \"○ [미세먼지] 전 권역이 '좋음'∼'보통'으로 예상됨.\" }";
     }
 
     // 지역별 등급 표시 요청
-    @GetMapping("/forecase/grade")
+    @GetMapping("/grade")
     public String getInformGrade() {
 
         return "{ \"informGrade\": \"서울 : 보통,제주 : 보통,전남 : 보통,전북 : 보통,광주 : 보통,경남 : 보통,경북 : 보통,울산 : 보통,대구 : 보통,부산 : 보통,충남 : 보통,충북 : 보통,세종 : 보통,대전 : 보통,영동 : 보통,영서 : 보통,경기남부 : 보통,경기북부 : 보통,인천 : 보통\" }";
--- BE/src/main/java/com/codesquad/dust08/controller/StationController.java
+++ BE/src/main/java/com/codesquad/dust08/controller/StationController.java
@@ -1,21 +1,23 @@
 package com.codesquad.dust08.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/stations")
 public class StationController {
     // 위도, 경도에서 가까운 측정소 반환
-    @GetMapping("/stations")
-    public String getStationName(@RequestParam double latitude, @RequestParam double longitude) {
+    @GetMapping("")
+    public String getStationName(double latitude, double longitude) {
 
         return "{ \"stationName\" : \"강남구\" }";
     }
 
     // 특정 측정소의 최근 측정된 미세먼지 등급 반환
-    @GetMapping("/stations/dust-status")
-    public String getGrade(@RequestParam String stationName) {
+    @GetMapping("/dust-status")
+    public String getGrade(String stationName) {
 
         return "{ \"pm10Value\": \"68\", \"pm10Grade1h\": \"2\", \"dataTime\": \"2020-03-31 09:00\" }";
     }
