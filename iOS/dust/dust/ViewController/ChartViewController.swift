878a4ccab0dc949f6f252b9370025544b932f30d
1Consumption <hanseop95@gmail.com>
2020-03-31 16:15:06 +0900

    [Feat] 모델 생성
    모델을 생성해서 테이블 뷰 데이터소스에 참조를 넘겨 줌

--- iOS/dust/dust/ViewController/ChartViewController.swift
+++ iOS/dust/dust/ViewController/ChartViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ChartViewController: UIViewController {
 
+    @IBOutlet weak var chartTableView: UITableView!
+    var model = [DustInfoModel]()
+    private var dataSource = ChartTableViewDatasource()
+    private var delegate = ChartTableViewDelegate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for _ in 0..<24 {
+            model.append(DustInfoModel(numeric: Int.random(in: 0...200)))
+        }
+        
+        dataSource.model = model
+        self.chartTableView.dataSource = dataSource
     }
 }
 
