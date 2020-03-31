aadaa34317966c29eff96352d906954db38647aa
1Consumption <hanseop95@gmail.com>
2020-03-31 12:41:05 +0900

    [Feat] 커스텀 뷰 생성
    state 값을 변경하면 배경 색이 변하도록 만드는 커스텀 클래스 생성

--- iOS/dust/dust/View/SummaryView.swift
+++ iOS/dust/dust/View/SummaryView.swift
@@ -9,15 +9,39 @@
 import UIKit
 
 class SummaryView: UIView {
-    var gradientLayer: CAGradientLayer
+    
+    enum State: String {
+        case Good
+        case Normal
+        case Bad
+        case Terrible
+    }
+    
+    private var gradientLayer: CAGradientLayer
+    private var state: State {
+        didSet {
+            setGradientColor(state: state)
+        }
+    }
     
     override init(frame: CGRect) {
         self.gradientLayer = CAGradientLayer()
+        self.state = .Normal
         super.init(frame: frame)
+        self.gradientLayer.frame = self.bounds
     }
     
     required init?(coder: NSCoder) {
         self.gradientLayer = CAGradientLayer()
+        self.state = .Normal
         super.init(coder: coder)
+        self.gradientLayer.frame = self.bounds
+    }
+    
+    func setGradientColor(state: State) {
+        DispatchQueue.main.async {
+            self.gradientLayer.colors = [UIColor(named: state.rawValue)?.cgColor ?? UIColor.white.cgColor, UIColor.white.cgColor]
+            self.layer.insertSublayer(self.gradientLayer, at: 0)
+        }
     }
 }
