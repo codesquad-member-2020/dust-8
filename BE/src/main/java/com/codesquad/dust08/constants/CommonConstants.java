2c1319bdf4c1dd7a901651b47e36f02296ae9109
blossun <dnsngu@gmail.com>
2020-04-02 04:21:19 +0900

    [Feat] 입력한 위도 경도에서 가장 가까운 측정소 반환 기능 구현
    위경도 → TM 좌표 변환을 위해 카카오 API를 이용

--- BE/src/main/java/com/codesquad/dust08/constants/CommonConstants.java
+++ BE/src/main/java/com/codesquad/dust08/constants/CommonConstants.java
@@ -2,5 +2,7 @@ package com.codesquad.dust08.constants;
 
 public class CommonConstants {
     public static final String SERVICE_KEY = "6aRmxVKhQasLEppxZRQirEm2LIgzObFzmhH4sg1veRb3trWgiOU58lfEQqUHcYMucI398cs2Vd8S2Ygz9pS9Zw%3D%3D";
+    public static final String KAKAO_KEY = "KakaoAK 25f8770b6635cded6d0f56f88bee67ce";
     public static final String ENCODING_TYPE = "UTF-8";
+    public static final String CONTENT_TYPE_JSON = "application/json";
 }
--- BE/src/main/java/com/codesquad/dust08/constants/UrlConstants.java
+++ BE/src/main/java/com/codesquad/dust08/constants/UrlConstants.java
@@ -2,4 +2,6 @@ package com.codesquad.dust08.constants;
 
 public class UrlConstants {
     public static final String STATION_DUST_STATUS = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty";
+    public static final String NEAR_STATION = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList";
+    public static final String KAKAO_MAP_CONVERT_TRANSCOORD = "https://dapi.kakao.com/v2/local/geo/transcoord";
 }
