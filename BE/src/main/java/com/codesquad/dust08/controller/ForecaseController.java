b96786bdbe9a3fc4b3f19cadf1f6ca13e9363592
ragdoll <>
2020-04-01 23:49:30 +0900

    [Feat] 대기 예보 문구에 대한 OPEN API 데이터 불러오기 구현
    커스텀 요청 파라미터에 대한 대기 예보 문구 출력 확인

--- BE/src/main/java/com/codesquad/dust08/controller/ForecaseController.java
+++ BE/src/main/java/com/codesquad/dust08/controller/ForecaseController.java
@@ -1,10 +1,21 @@
 package com.codesquad.dust08.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/forecase")
@@ -22,9 +33,35 @@ public class ForecaseController {
 
     // 예보 문구 출력 호출
     @GetMapping("/informoverall")
-    public String getOverallForecase() {
+    public String getOverallForecase() throws IOException {
+        String today = LocalDate.now().toString();
 
-        return "{ \"informOverall\" : \"○ [미세먼지] 전 권역이 '좋음'∼'보통'으로 예상됨.\" }";
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=FwHUBCLVF%2BGTXPUDF9To8ArFacN6La84DaSvEn5dP4Jjw%2BPR9VUd44iYd2ITMu0yO88yseP0akLxoUvsKcHAow%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("searchDate","UTF-8") + "=" + URLEncoder.encode(today, "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("informCode","UTF-8") + "=" + URLEncoder.encode("PM10", "UTF-8")); /*페이지 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("_returnType","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*페이지 번호*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+
+        return sb.toString();
     }
 
     // 지역별 등급 표시 요청
