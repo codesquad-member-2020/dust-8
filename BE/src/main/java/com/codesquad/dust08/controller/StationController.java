1cc3cb0c9cf2fd4922e95ed64ffa376f42aa2637
blossun <dnsngu@gmail.com>
2020-04-01 17:58:28 +0900

    [Feat] open api로 측정소별 실시간 측정정보 받아오는 기능 구현
    HttpURLConnection 으로 api 응답을 받아온다.
    하루치(Daily), 24개의 데이터를 json 타입으로 받아옴
    Closed issue #24

--- BE/src/main/java/com/codesquad/dust08/controller/StationController.java
+++ BE/src/main/java/com/codesquad/dust08/controller/StationController.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 @RestController
 @RequestMapping("/stations")
 public class StationController {
@@ -19,7 +27,37 @@ public class StationController {
 
     // 특정 측정소의 최근 측정된 미세먼지 등급 반환
     @GetMapping("/dust-status")
-    public String getGrade(String stationName) {
+    public String getGrade(String stationName) throws IOException {
+        final String SERVICE_KEY = "6aRmxVKhQasLEppxZRQirEm2LIgzObFzmhH4sg1veRb3trWgiOU58lfEQqUHcYMucI398cs2Vd8S2Ygz9pS9Zw%3D%3D";
+        log.debug("stationName : {}", stationName);
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty"); //URL
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + SERVICE_KEY); //ServiceKey
+        urlBuilder.append("&" + URLEncoder.encode("stationName", "UTF-8") + "=" + stationName); //측정소
+        urlBuilder.append("&" + URLEncoder.encode("dataTerm", "UTF-8") + "=" + URLEncoder.encode("DAILY", "UTF-8")); //하루치 데이터
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("24", "UTF-8")); //24개 데이터
+        urlBuilder.append("&" + URLEncoder.encode("_returnType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8"));
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        int responseCode = conn.getResponseCode();
+        log.debug("Response code : {}", responseCode);
+        BufferedReader br;
+        if (responseCode >= 200 && responseCode <= 300) {
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+        br.close();
+        conn.disconnect();
+        log.debug("Response : {}", sb.toString());
 
         return "{\n" +
                 "\t\"dustStatus\": [{\n" +
