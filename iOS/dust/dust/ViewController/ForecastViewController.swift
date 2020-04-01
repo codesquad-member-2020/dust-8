3b10cdeb2ef0b598ded5e67bcffb37a462ecd04a
1Consumption <hanseop95@gmail.com>
2020-04-02 02:44:02 +0900

    [Feat] 사진 슬라이드 동작 추가
    UISlider를 움직이면 사진이 바뀌도록 기능 추가

--- iOS/dust/dust/ViewController/ForecastViewController.swift
+++ iOS/dust/dust/ViewController/ForecastViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ForecastViewController: UIViewController {
     
-    @IBOutlet weak var playButton: UIButton!
-    @IBOutlet weak var forecastImageVIew: UIImageView!
     private let imageManager = ImageManager()
+    
+    @IBOutlet weak var forecastImageVIew: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        DispatchQueue.main.async {
+            self.forecastImageVIew.image = self.imageManager.index(of: Int(sender.value))
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +32,15 @@ class ForecastViewController: UIViewController {
     @objc func downloadFinished() {
         DispatchQueue.main.async {
             self.forecastImageVIew.image = self.imageManager.index(of: 0)
+            self.slider.setupSlider(min: 0, max: Float(self.imageManager.count() - 1))
         }
+    }
+}
+
+extension UISlider {
+    func setupSlider(min: Float, max: Float) {
+        self.value = 0
+        self.maximumValue = max
+        self.minimumValue = min
     }
 }
