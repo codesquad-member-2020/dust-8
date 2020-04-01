3334af696e0f9dead961f0eb3608ffc75f02bace
1Consumption <hanseop95@gmail.com>
2020-04-02 02:37:20 +0900

    [Fix] URLSession 인스턴스 선언 방식 변경
    shared 프로퍼티를 사용하여 싱글톤을 적용함

--- iOS/dust/dust/Model/NetworkConnection.swift
+++ iOS/dust/dust/Model/NetworkConnection.swift
@@ -10,23 +10,17 @@ import Foundation
 
 class NetworkConnection {
     class func request(resource: String, handlder: @escaping (Data) -> Void){
-        let defaultSession = URLSession(configuration: .default)
         guard let url = URL(string: resource) else {
             return
         }
         
-        let request = URLRequest(url: url)
-        
-        let dataTask = defaultSession.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+        let dataTask = URLSession.shared.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
             guard error == nil else {
                 print("Error occur: \(String(describing: error))")
-                
                 return
             }
             
-            guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                return
-            }
+            guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 else { return}
             
             handlder(data)
         }
