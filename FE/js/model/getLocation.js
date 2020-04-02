c6c11aa85e4e4a0192e3aa53780e9551e0a082e2
choisohyun <sohyun4283@naver.com>
2020-04-02 22:02:10 +0900

    [Fix] GPS 위도, 경도 얻어올 때 예외처리 추가

--- FE/js/model/getLocation.js
+++ FE/js/model/getLocation.js
@@ -5,20 +5,30 @@ import { getDustStatus } from "./getDustStatus.js";
 const getPosition = () => {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject);
-  }).then(pos => {
-    return {
-      latitude: pos.coords.latitude,
-      longitude: pos.coords.longitude
-    };
-  });
+  })
+    .then(pos => {
+      return {
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude
+      };
+    })
+    .catch(() => {
+      return {
+        latitude: 37.491076,
+        longitude: 127.033353
+      };
+    });
 };
 
 const fetchLocation = () => {
   getPosition().then(position => {
-    return fetchRequest(process.env.MOCK_URL + process.env.LOCATION, position)
+    return fetchRequest("http://192.168.1.42:8080/" + process.env.SERVICELOCATION, position)
       .then(stationName => {
-        renderLocation(stationName.stationName);
-        getDustStatus(stationName.stationName);
+        if (!stationName.result) {
+          alert(stationName.errorMessage);
+        }
+        renderLocation(stationName.result);
+        getDustStatus(stationName.result);
       })
       .catch(error => console.error(error));
   });
