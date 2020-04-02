fe9459631d53418fac79c92be545118688ac037c
choisohyun <sohyun4283@naver.com>
2020-04-02 21:10:57 +0900

    [Feat] 현재 시간 랜더링, 시간대에 따라 오늘/어제 다르게 표시하게 추가

--- FE/js/dustView/dustView.js
+++ FE/js/dustView/dustView.js
@@ -1,22 +1,44 @@
 import { qs$ } from "../lib/util.js";
-import { GRADE, gradeList } from "../common/constants.js";
+import { GRADE, gradeList, currentHours } from "../common/constants.js";
 
 const renderLocation = location => {
   qs$(".info-location").innerText = location;
 };
 
+const renderCurrentHours = time => {
+  qs$(".statistics-time").innerText = time;
+
+  if (time > currentHours) {
+    qs$(".statistics-time").setAttribute("data-before", "어제");
+    return;
+  }
+  qs$(".statistics-time").setAttribute("data-before", "오늘");
+};
+
 const renderCharts = numerics => {
-  numerics.forEach(({ pm10Value, pm10Grade1h }) => {
+  let targetInfo1;
+  let targetInfo2;
+
+  numerics.forEach(({ dataTime, pm10Value, pm10Grade1h }) => {
+    const currentGrade = GRADE[gradeList[pm10Grade1h - 1]];
+
     qs$(".chart--horiz").innerHTML += `
     <li class="chart__bar" style="width: ${Number(pm10Value) / 2}%; background: ${
-      GRADE[gradeList[pm10Grade1h - 1]].COLOR
+      currentGrade.COLOR
     }">
       <span class="chart__label">
         ${pm10Value}
       </span>
     </li>
     `;
+
+    if (new Date(dataTime).getHours() === currentHours) {
+      targetInfo1 = currentGrade;
+      targetInfo2 = pm10Value;
+    }
   });
+
+  renderDustInfo(targetInfo1, targetInfo2);
 };
 
 export { renderLocation, renderCharts };
