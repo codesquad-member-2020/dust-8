1fa2cb870f4939d25968d642f38d037172f6f03c
1Consumption <hanseop95@gmail.com>
2020-04-03 03:15:31 +0900

    [Feat] 차트 UI 적용
    현재 row에 맞는 모델 정보를 cell에 주입

--- iOS/dust/dust/View/ChartTableView/ChartTableViewDatasource.swift
+++ iOS/dust/dust/View/ChartTableView/ChartTableViewDatasource.swift
@@ -18,7 +18,10 @@ class ChartTableViewDatasource: NSObject, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChartCell") as! ChartTableViewCell
-        
+        let currentModel = modelManger?.index(of: indexPath.row) ?? DustInfoModel()
+        cell.numeric.text = String(currentModel.numeric)
+        cell.bar.frame = CGRect(x: 0, y: 0, width: cell.bounds.width * CGFloat(currentModel.numeric) / CGFloat(200), height: cell.bounds.height)
+        cell.bar.backgroundColor = UIColor(named: DustGrade.Grade(index: currentModel.grade).rawValue) ?? UIColor.white
         return cell
     }
 }
